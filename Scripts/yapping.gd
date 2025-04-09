fc486b909ffcb2dc6e8e4cef98429047003f140b
Sydney Maller <sydneymaller@gustavus.edu>
2025-04-08 19:34:58 -0500

    Same as before just with Icons 👍
    icons are there now

--- Scripts/yapping.gd
+++ Scripts/yapping.gd
@@ -2,14 +2,6 @@ extends Control
 
 @onready var click = $AudioStreamPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_go_to_canvas_pressed() -> void:
