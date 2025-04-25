3f9168129d92f848d3dd3361c52c4fc07d6d437f
Sydney Maller <sydneymaller@gustavus.edu>
2025-04-24 20:31:03 -0500

    fixed music_loop

--- Scripts/music_player.gd
+++ Scripts/music_player.gd
@@ -6,17 +6,18 @@ var volume_slider : HSlider
 func _ready() -> void:
 	audio_player = AudioStreamPlayer.new()
 	add_child(audio_player)
-	
 	var music = preload("res://Music/mbg main theme demo.mp3")
 	audio_player.stream = music
 	audio_player.play()
-	
+
+	# Enable looping
+	audio_player.stream.loop = true  # Set the loop property to true for the music
+
 	audio_player.volume_db = linear_to_db(1.0)
-	
-	
+
 func set_volume(value):
-	var clamped_value = clamp(value,0.0,1.0)
+	var clamped_value = clamp(value, 0.0, 1.0)
 	audio_player.volume_db = linear_to_db(clamped_value)
-	
+
 func linear2db(linear):
 	return 20 * log(linear)
--- Scripts/volume_slider.gd
+++ Scripts/volume_slider.gd
@@ -2,21 +2,16 @@ extends Control
 
 @onready var volume_slider = $VolumeSlider
 @onready var button = $AudioStreamPlayer
-
 @onready var back_button = $BackButton
 
 var is_playing = false 
 
 func _ready() -> void:
-	volume_slider = 1.0
-	
+	volume_slider.value = 0.5  # Set the slider to the middle value
+	MusicPlayer.set_volume(0.5)  # Optional: also set volume at start
 
 func _on_volume_slider_value_changed(value: float) -> void:
 	MusicPlayer.set_volume(value)
-	
-	
-	
-
 
 func _on_back_button_pressed() -> void:
 	if is_playing:
